c39b8d40de4458bca27d9c62d14f2b71f025831b
Sumit <sumitvekariya7@gmail.com>
2020-09-19 11:05:12 +0530

    Implementation of Stripe integration in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { SuccessComponent } from './success/success.component';
+import { FailureComponent } from './failure/failure.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    SuccessComponent,
+    FailureComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: ProductComponent
+      },
+      {
+        path: 'success',
+        component: SuccessComponent
+      },
+      {
+        path: 'failure',
+        component: FailureComponent
+      },
+      {
+        path: '**',
+        component: ProductComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  stripe_key: 'pk_test_51HSxmHFHabj9XRH6FRDABkTg4M5jSU7ZPKXismUzYytZh8JkfxqiwjtVDSlbcAR4kQ8U6Yy19RQCoSjsspXVmhzf002G4ny8sA'
 };
 
 /*
